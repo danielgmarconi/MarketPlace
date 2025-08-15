d9afcff64c3f421b9438f753bff90cdb7f20581a
danielgmarconi <danielgmarconi@gmail.com>
2025-08-15 15:42:14 -0300

    Implementações

--- back-end/MarketPlace.Application/Validators/UserProfileValidator.cs
+++ back-end/MarketPlace.Application/Validators/UserProfileValidator.cs
@@ -17,44 +17,41 @@ namespace MarketPlace.Application.Validators
                     .MaximumLength(150).WithMessage(messageLocalizer.Get("Min-Value", "FullName", "150"));
                 RuleFor(x => x.Email)
                     .Must(x => !string.IsNullOrEmpty(x)).WithMessage(messageLocalizer.Get("Is-Required", "Email"))
-                    .EmailAddress().WithMessage(messageLocalizer.Get("Is-Required", "Email"));
+                    .EmailAddress().WithMessage(messageLocalizer.Get("Invalid", "Email"));
                 RuleFor(x => x.Password)
                     .Must(x => !string.IsNullOrEmpty(x)).WithMessage(messageLocalizer.Get("Is-Required", "Password"))
                     .Matches(RegexValidator.Password)
                     .WithMessage(messageLocalizer["Invalid-Password"]);
                 RuleFor(x => x)
-                    .Must(x => userRepository.Get(x.Email).Result == null).WithMessage("Email already exists.");
+                    .Must(x => userRepository.Get(x.Email).Result == null).WithMessage(messageLocalizer["Email-Exists"]);
             });
             RuleSet("Update", () => {
                 RuleFor(x => x)
-                    .Must(x => x == null || x.Id > 0).WithMessage("Invalid Id.");
+                    .Must(x => x == null || x.Id < 1).WithMessage(messageLocalizer.Get("Invalid", "Id"));
                 RuleFor(x => x.FullName)
                     .Must(x => !string.IsNullOrEmpty(x)).WithMessage(messageLocalizer.Get("Is-Required", "FullName"))
-                    .MinimumLength(7).When(x => !string.IsNullOrEmpty(x.FullName)).WithMessage("The FullName must be at least 7 characters long.")
-                    .MaximumLength(150).When(x => !string.IsNullOrEmpty(x.FullName)).WithMessage("The FullName must have a maximum of 150 characters.");
+                    .MinimumLength(7).WithMessage(messageLocalizer.Get("Min-Value", "FullName", "7"))
+                    .MaximumLength(150).WithMessage(messageLocalizer.Get("Min-Value", "FullName", "150"));
                 RuleFor(x => x.Email)
                     .Must(x => !string.IsNullOrEmpty(x)).WithMessage(messageLocalizer.Get("Is-Required", "Email"))
-                    .EmailAddress().WithMessage("Email is required.");
+                    .EmailAddress().WithMessage(messageLocalizer.Get("Invalid", "Email"));
                 RuleFor(x => x)
-                   .Must(x => ValidateEmailUpdate(userRepository, x)).WithMessage("Email is different from the one registered");
+                   .Must(x => ValidateEmailUpdate(userRepository, x)).WithMessage(messageLocalizer["Email-Different-Registered"]);
                 RuleFor(x => x)
                     .Must(x => ValidatePasswordUpdate(x))
-                    .WithMessage("The Password must be between 8 and 20 characters long, including at least one uppercase letter, one lowercase letter, one number, and one special character.");
-
+                    .WithMessage(messageLocalizer["Invalid-Password"]);
             });
-
             RuleSet("ChangePassword", () => {
                 RuleFor(x => x)
                     .Must(x => x.Id != null || !string.IsNullOrWhiteSpace(x.UserGuid))
-                    .WithMessage("Enter Id or UserGuid.");
+                    .WithMessage(messageLocalizer["Enter-Id-UserGuid"]);
                 RuleFor(x => x.UserGuid)
                     .Must(x => ValidateGuid(x))
-                    .WithMessage("Invalid UserGuid.");
+                    .WithMessage(messageLocalizer.Get("Invalid", "UserGuid"));
                 RuleFor(x => x.Password)
-                    .NotNull().WithMessage("Password is required.")
-                    .NotEmpty().WithMessage("Password is required.")
+                    .Must(x => string.IsNullOrWhiteSpace(x)).WithMessage(messageLocalizer.Get("Is-Required", "Password"))
                     .Matches(RegexValidator.Password)
-                    .WithMessage("The Password must be between 8 and 20 characters long, including at least one uppercase letter, one lowercase letter, one number, and one special character.");
+                    .WithMessage(messageLocalizer["Invalid-Password"]);
 
             });
         }
